b79af1f08710a7b7ce48164cd0e9c5c985894637
Robert Luisi <bobby.luisi@gmail.com>
2019-06-20 02:32:28 -0400

    Init

---
+++ chirpblastapplication/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ChirpblastapplicationConfig(AppConfig):
+    name = 'chirpblastapplication'
---
+++ chirpblastapplication/views.py
@@ -0,0 +1,25 @@
+from django.http import HttpResponse
+from django.views.generic import TemplateView
+from .services.web.restservice import WebRequest
+
+class IndexView(TemplateView):
+    template_name = 'chirpblastapplication/index.html'
+
+    def __init__(self):
+        self.__testJson= self.Test()
+        self.__testData = WebRequest.parse_JSON(self.__testJson)
+        print(self.__testData)
+
+    def get_context_data(self, **kwargs):
+        context = super(IndexView, self).get_context_data(**kwargs)
+        context.update({'data': self.__testJson})
+        return context
+
+    def Test(sefl):
+        try:
+            webrequest = WebRequest('https://freegeoip.app/json/')
+            return webrequest.get()
+        except:
+            return 0
+
+    
